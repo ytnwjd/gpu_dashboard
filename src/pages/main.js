5318ec36e7fb9c48eb79457b306909b91adff918
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-14 21:07:39 +0900

    feat: main buttons

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,64 @@
-import React from 'react';
-import CustomCard from '../components/CustomCard';
+import React, {useState} from 'react';
+import JobListCard from "../components/JobListCard";
+import InfoCard from "../components/InfoCard";
+import FormCard from "../components/FormCard";
+import { Check as CheckIcon, PlayArrow as PlayArrowIcon } from '@mui/icons-material';
+import RoundedButton from "../components/Button/RoundedButton";
+
+import './mainPage.css';
 
 const MainPage = () => {
+    const [showDescription, setShowDescription] = useState(false);
+    const [descriptionText, setDescriptionText] = useState('');
+
+    const handleRunOrWait = () => {
+        console.log("실행 또는 대기하기 버튼 클릭됨");
+        setDescriptionText("남은 GPU가 없는 경우 대기열로 들어갑니다.");
+        setShowDescription(true);
+    };
+
+    const handleCheck = () => {
+        console.log("검사하기 버튼 클릭됨");
+        setDescriptionText("검사에 성공하였습니다.");
+        setShowDescription(true);
+    };
+
     return (
-        <div style={{ padding: 10 }}>
-            <CustomCard
-                title="React + MUI Card"
-                content="이것은 커스텀 카드 컴포넌트입니다. MUI를 기반으로 구성되었습니다."
-                label= {"저장"}
-                width = '420px'
-                height = '520px'
-            />
+        <div className="main-page-container">
+
+            <div className="left-section">
+                <FormCard />
+                <div className="btns-container">
+                    <RoundedButton
+                        label="실행 또는 대기하기"
+                        width="180px"
+                        height="40px"
+                        state="enable"
+                        onClick={handleRunOrWait}
+                        icon={<PlayArrowIcon sx={{ fontSize: 20, marginRight: 1 }} />}
+                    />
+                    <RoundedButton
+                        label="검사하기"
+                        width="100px"
+                        height="40px"
+                        state="enable"
+                        onClick={handleCheck}
+                        icon={<CheckIcon sx={{ fontSize: 20, marginRight: 1 }} />}
+                    />
+                </div>
+
+                {showDescription && (
+                    <p className="description-text">
+                        {descriptionText}
+                    </p>
+                )}
+            </div>
+
+            <div className="right-section">
+                <InfoCard />
+                <JobListCard />
+            </div>
+
         </div>
     );
 };
