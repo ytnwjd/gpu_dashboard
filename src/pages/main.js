eb67633459ea8761723b2c9e99e1fa63b5d02c16
y.tnwjd <ysjsw@ajou.ac.kr>
2025-07-21 21:54:35 +0900

    style: remove btns

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,24 +2,14 @@ import React, {useState} from 'react';
 import JobListCard from "../components/Card/JobListCard";
 import InfoCard from "../components/Card/InfoCard";
 import FormCard from "../components/Card/FormCard";
-import { Check as CheckIcon, PlayArrow as PlayArrowIcon } from '@mui/icons-material';
-import RoundedButton from "../components/Button/RoundedButton";
-
 import './mainPage.css';
 
 const MainPage = () => {
     const [showDescription, setShowDescription] = useState(false);
     const [descriptionText, setDescriptionText] = useState('');
 
-    const handleRunOrWait = () => {
-        console.log("실행 또는 대기하기 버튼 클릭됨");
-        setDescriptionText("남은 GPU가 없는 경우 대기열로 들어갑니다.");
-        setShowDescription(true);
-    };
-
-    const handleCheck = () => {
-        console.log("검사하기 버튼 클릭됨");
-        setDescriptionText("검사에 성공하였습니다.");
+    const handleJobSubmitSuccess = (message) => {
+        setDescriptionText(message);
         setShowDescription(true);
     };
 
@@ -27,26 +17,8 @@ const MainPage = () => {
         <div className="main-page-container">
 
             <div className="left-section">
-                <FormCard />
-                <div className="btns-container">
-                    <RoundedButton
-                        label="실행 또는 대기하기"
-                        width="180px"
-                        height="40px"
-                        state="enable"
-                        onClick={handleRunOrWait}
-                        icon={<PlayArrowIcon sx={{ fontSize: 20, marginRight: 1 }} />}
-                    />
-                    <RoundedButton
-                        label="검사하기"
-                        width="100px"
-                        height="40px"
-                        state="enable"
-                        onClick={handleCheck}
-                        icon={<CheckIcon sx={{ fontSize: 20, marginRight: 1 }} />}
-                    />
-                </div>
-
+                <FormCard onJobSubmitSuccess={handleJobSubmitSuccess} />
+    
                 {showDescription && (
                     <p className="description-text">
                         {descriptionText}
